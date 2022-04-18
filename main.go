8ea0200ec31b8de0ac5c2560357b83ad63f69fae
YU-YUEHAO <992599433@qq.com>
2022-04-18 10:55:08 +0800

    “3.0"

--- main.go
+++ main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 
-	c"dapp/Connect"
-	"fmt"
+	// c"dapp/Connect"
+	// "fmt"
 	s"dapp/Han"
 )
 
 
 func main() {
     s.Start()
-	ins:=c.Getsmartcontract()
-	head,_:=c.GetBlockNumber()
-	_,adress:=c.Getaccout()
-	fmt.Println("contractBalance",c.GetcontractBanlance(ins,adress,head))
+	// ins:=c.Getsmartcontract()
+	// head,_:=c.GetBlockNumber()
+	// _,adress:=c.Getaccout()
+	// fmt.Println("contractBalance",c.GetcontractBanlance(ins,adress,head))
 
 }
